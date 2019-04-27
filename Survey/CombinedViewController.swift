e25c8309dcb170308dfd0f75382d89e44c27dd59
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-04-27 16:56:53 -0400

    added Combined code

--- Survey/CombinedViewController.swift
+++ Survey/CombinedViewController.swift
@@ -19,14 +19,17 @@ class CombinedViewController: UIViewController, VoteControllerProtocol {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ToVotingView" {
+            guard let destination = segue.destination as? VotingViewController else { return }
+            destination.voteController = voteController
+        } else if segue.identifier == "ToResultsTableView" {
+            guard let destination = segue.destination as? ResultsReTableViewController else { return }
+            destination.voteController = voteController
+        }
     }
-    */
 
 }
